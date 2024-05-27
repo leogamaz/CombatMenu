98a85ebd646436a6d44d582e65c9005aa311bd75
SkillBoot <leonardogama2000@outlook.com.br>
2024-05-26 20:44:38 -0400

    Adicionando iniciativa

--- src/app/shared/core/services/OBR/context-menu.service.ts
+++ src/app/shared/core/services/OBR/context-menu.service.ts
@@ -6,32 +6,52 @@ import OBR from '@owlbear-rodeo/sdk';
 })
 export class ContextMenuService {
 
-  ID = "com.tutorial.initiative-tracker"
+  ID: string = "com.tutorial.initiative-tracker";
 
   constructor() { }
 
   setupContextMenu(){
-    console.log('teste')
+    console.log('teste');
     OBR.contextMenu.create({
-      id:`${this.ID}/context-menu`,
-      icons:[
+      id: `${this.ID}/context-menu`,
+      icons: [
         {
           icon: "/add.svg",
           label: "Adicionar Player",
-          filter:{
-            every:[{key:"layer", value: "CHARACTER"}]
+          filter: {
+            every: [{ key: "layer", value: "CHARACTER" },{ key: ["metadata", `${this.ID}/metadata`], value: undefined }],
+
           }
-        }
+        },
+        {
+          icon: "/remove.svg",
+          label: "Remover Jogador",
+          filter: {
+            every: [{ key: "layer", value: "CHARACTER" }],
+          },
+        },
       ],
-      onClick(context){
-        const initiative = 15
-        OBR.scene.items.updateItems(context.items, (items) =>{
+      onClick: (context) => {  // Use arrow function to preserve the 'this' context
+        const addToInitiative = context.items.every((item)=> item.metadata[`${this.ID}/metadata`] === undefined);
+        if (addToInitiative){
+          const initiativee = 15;
+        OBR.scene.items.updateItems(context.items, (items) => {
           for (let item of items){
 
+            item.metadata[`${this.ID}/metadata`] = {
+              initiativee,
+            }
+            
           }
-        })
+        });
+        }else{
+          OBR.scene.items.updateItems(context.items, (items) => {
+            for (let item of items) {
+              delete item.metadata[`${this.ID}/metadata`]
+            }
+          });
+        }
       },
-    })
+    });
   }
-
 }
