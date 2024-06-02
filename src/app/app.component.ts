765b59f1022a1af023a4de7a9f5e1a8d07f2e297
SkillBoot <leonardogama2000@outlook.com.br>
2024-06-01 21:47:40 -0400

    Criando menu de contexto novamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,54 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, contentChild } from '@angular/core';
 
 import OBR, { Metadata } from '@owlbear-rodeo/sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'status-character';
-  metadata = 'com.tutorial.initiative-tracker/metadata'
+  metadata = 'com.simple.combat.menu/metadata';
+  contextMenu = 'com.simple.combat.menu/contextMenu';
+  id = 'com.simple.combat.menu';
 
-  constructor() {}  // Certifique-se de injetar o serviço no construtor
+
+
+  constructor() {} // Certifique-se de injetar o serviço no construtor
 
   ngOnInit(): void {
-      OBR.onReady(() => {
+    const metadata = this.metadata
+    OBR.onReady(() => {
+      OBR.contextMenu.create({
+        id: this.contextMenu,
+        icons:[
+          {
+            icon: 'add.svg',
+            label: "Adicionar ao Combate",
+            filter: {
+              every:[
+                {key: "layer", value: "CHARACTER"},
+                {key: ["metadata",metadata],value: undefined}
+              ]
+            }
 
-
+          },
+          {
+            icon: 'remove.svg',
+            label: "Remover do Combate",
+            filter: {
+              every:[
+                {key: "layer", value: "CHARACTER"},
+              ]
+            }
+          }
+        ],
+        onClick(context){
+          const addToMenu = context.items.every((item) => item.metadata[metadata] === undefined);
+          
+        }
       })
+    });
   }
 }
