6577a3457d70b65d8e39eb2182ac6199cf2aca3a
SkillBoot <leonardogama2000@outlook.com.br>
2024-06-01 21:03:52 -0400

    Inicio nova estrategia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ContextMenuService } from './shared/core/services/OBR/context-menu/context-menu.service';
+
 import OBR, { Metadata } from '@owlbear-rodeo/sdk';
-import { ListaPlayersService } from './shared/core/services/OBR/lista-players/lsita-players.service';
-import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,16 +11,12 @@ export class AppComponent implements OnInit{
   title = 'status-character';
   metadata = 'com.tutorial.initiative-tracker/metadata'
 
-  constructor(private contextMenuService: ContextMenuService, private listaPlayersService: ListaPlayersService) {}  // Certifique-se de injetar o serviço no construtor
+  constructor() {}  // Certifique-se de injetar o serviço no construtor
 
   ngOnInit(): void {
       OBR.onReady(() => {
-        this.contextMenuService.setupContextMenu();
-        this.listaPlayersService.setupInitiativeList();
+
 
       })
   }
-
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CombatMenuPlayerComponent } from './shared/components/combat-menu-player/combat-menu-player.component';
-import { UpdatePlayersService } from './shared/core/services/OBR/update-players/update-players.service';
-import { ListaPlayersService } from './shared/core/services/OBR/lista-players/lsita-players.service';
-import { ContextMenuService } from './shared/core/services/OBR/context-menu/context-menu.service';
 
 @NgModule({
-  declarations: [AppComponent, CombatMenuPlayerComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [
-    UpdatePlayersService,
-    ListaPlayersService,
-    ContextMenuService,
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
